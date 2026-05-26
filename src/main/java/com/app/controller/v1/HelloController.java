f2e06a52ac8b3a13e8bd2527c44042287e334adb
DevMaxiRivas <maximilianorivas01@gmail.com>
2026-05-26 09:09:43 -0300

    fix(controller): The test controller has been updated

--- src/main/java/com/app/controller/v1/HelloController.java
+++ src/main/java/com/app/controller/v1/HelloController.java
@@ -1,9 +1,11 @@
-package com.app.controller;
+package com.app.controller.v1;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/api/v1/greet")
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
