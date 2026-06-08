617dfae61758e691b415e864f209d14f5dfc6866
DevMaxiRivas <maximilianorivas01@gmail.com>
2026-06-08 14:11:57 -0300

    fix(service): Unnecessary imports have been deleted

--- src/main/java/com/app/service/user/impl/UserServiceImpl.java
+++ src/main/java/com/app/service/user/impl/UserServiceImpl.java
@@ -2,19 +2,17 @@ package com.app.service.user.impl;
 
 import com.app.dto.v1.user.QueryParamsUserFilterDTO;
 import com.app.dto.v1.user.UserResponseDTO;
+import com.app.exception.resource.ResourceNotFoundCustomException;
 import com.app.mapper.user.UserMapper;
 import com.app.model.User;
 import com.app.repository.UserRepository;
 import com.app.service.user.UserService;
 import com.app.specification.user.UserSpecification;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.data.jpa.domain.Specification;
-
-import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
@@ -25,13 +23,13 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Long id) {
         return repo.findById(id)
-                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+                .orElseThrow(() -> new ResourceNotFoundCustomException("User Not Found", "url.param"));
     }
 
     @Override
     public User findByEmail(String email) {
         return repo.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("Email no registrado"));
+                .orElseThrow(() -> new ResourceNotFoundCustomException("User Not Found", "url.param"));
     }
 
     public Page<UserResponseDTO> getUsersPageable(Pageable pageable) {
